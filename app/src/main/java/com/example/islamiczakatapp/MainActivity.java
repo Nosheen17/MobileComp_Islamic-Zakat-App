021abcb04fc8f10687a641e7266f325c1a2bb2a0
Nosheen17 <hayat.mehreen17@gmail.com>
2022-05-01 10:34:36 +0500

    declaring and instantiating widgets

--- app/src/main/java/com/example/islamiczakatapp/MainActivity.java
+++ app/src/main/java/com/example/islamiczakatapp/MainActivity.java
@@ -1,14 +1,36 @@
-package com.example.islamiczakatapp;
+package com.firstapp.islamicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    Button Zakkat;
+    Button Fitrana;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Zakkat=(Button) findViewById(R.id.Zakwat);
+        Zakkat.setOnClickListener(this);
+        Fitrana=(Button) findViewById(R.id.Fitrana);
+        Fitrana.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.Zakwat:
+                startActivity(new Intent(MainActivity.this, ZakwatPage.class));
+                break;
+            case R.id.Fitrana:
+                startActivity(new Intent(MainActivity.this, activity_Fitrana.class));
+                break;
+        }
     }
 }
